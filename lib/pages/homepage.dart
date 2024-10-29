7dc13b071156eb165c715aa315693e8eca25f8fd
Juno-whut <owenrupnow17@gmail.com>
2024-10-29 18:00:42 -0400

    last commit message is for this commit

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -1,19 +1,126 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:voting_app/service/firebase_service.dart';
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
+
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
   @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  // firestore
+  final FirestoreService firestoreService = FirestoreService();
+
+  // text controller
+  final TextEditingController textController = TextEditingController();
+
+  // open dialog box to add note
+  void openTopicBox({String? docID}) {
+    showDialog(context: context, builder: (context) => AlertDialog(
+      content: TextField(
+        controller: textController,
+        ),
+        actions: [
+          // button to save
+          ElevatedButton(
+            onPressed: () {
+              // update or add new note
+              if (docID == null) {
+                firestoreService.addTopic(textController.text);
+              } else {
+                firestoreService.updateTopic(docID, textController.text);
+              }
+               
+              // clear text field
+              textController.clear();
+
+              // close dialog boc
+              Navigator.pop(context);
+            }, 
+            child: const Text('Add')
+      )
+        ]
+        ),
+      );
+    }
+
+    @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Voting App"),
+        title: const Text(
+          'Topics',
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 32,
+          ),
+          ),
         centerTitle: true,
-      ),
+        backgroundColor: Colors.deepPurpleAccent,
+        ),
+
       floatingActionButton: FloatingActionButton(
-        onPressed: () {},
+        onPressed: openTopicBox,
         child: const Icon(Icons.add),
-      ),
-    );
+        ),
+        body: StreamBuilder<QuerySnapshot>(
+          stream: firestoreService.getTopicsStream(),
+          builder: (context, snapshot) {
+            // if we have data, get all docs
+            if (snapshot.hasData) {
+              List topicList = snapshot.data!.docs;
+
+              // display as list
+              return ListView.builder(
+                itemCount: topicList.length,
+                itemBuilder: (context, index) {
+                  // get doc
+                  DocumentSnapshot document = topicList[index];
+                  String docID = document.id;
+
+                  // get note in doc
+                  Map<String, dynamic> data = document.data() as Map<String, dynamic>;
+                  String topicText = data['topic'];
+
+                  // display as list tile
+                  return ListTile(
+                    title: Text(topicText),
+                    trailing: Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        // upvote button
+                        IconButton(
+                          onPressed: () => firestoreService.upvoteTopic(docID),
+                          icon: const Icon(Icons.arrow_upward),
+                          ),
+                        // downvote button
+                        IconButton(
+                          onPressed: () => firestoreService.downvoteTopic(docID),
+                          icon: const Icon(Icons.arrow_downward),
+                        ),
+                        // update button
+                        IconButton(
+                          onPressed: () => openTopicBox(docID: docID), 
+                          icon: const Icon(Icons.settings),
+                        ),
+                      // delete button
+                        IconButton(
+                          onPressed: () => firestoreService.deleteTopic(docID), 
+                          icon: const Icon(Icons.delete),
+                        ),
+                    ])
+                  );
+                },
+              );
+            } else {
+              return const Text("No Topics...");
+            }
+          },
+        ),
+      );
+    }
   }
-}
+  
